ef58c3f6d9cb5279c410c9e24ccddae0db079f08
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-02 11:01:57 +0530

    Update Youtube.py

--- AnonXMusic/platforms/Youtube.py
+++ AnonXMusic/platforms/Youtube.py
@@ -10,11 +10,11 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from AnonxMusic.utils.formatters import time_to_seconds
+from AnonXMusic.utils.formatters import time_to_seconds
 
 
 def cookies():
-    cookie_dir = "YukkiMusic/utils/cookies"
+    cookie_dir = "cookies"
     cookies_files = [f for f in os.listdir(cookie_dir) if f.endswith(".txt")]
 
     cookie_file = os.path.join(cookie_dir, random.choice(cookies_files))
